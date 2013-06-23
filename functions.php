fa742445559b1d3c264c1a4230337e163c457591
Nick Ruiz <nick@fatcatonline.net>
2013-06-23 14:01:03 -0700

    Added ecommerce

--- functions.php
+++ functions.php
@@ -1,9 +1,25 @@
 <?php
 
-//Adding Featured Image for Posts
+// Adding Featured Image for Posts
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails' );
     set_post_thumbnail_size( 400, false );
 }
 
+// Woocommerce
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="main">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
+
+add_theme_support( 'woocommerce' );
 ?>
