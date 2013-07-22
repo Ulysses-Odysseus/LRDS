841f1b42989a8fdbc907f2ef0ad4a2d4bf5d5066
Nick Ruiz <nick@fatcatonline.net>
2013-07-22 13:52:30 -0700

    back up changes for mobile

--- about.php
+++ about.php
@@ -2,39 +2,31 @@
 /*
 Template Name: about
 */
+
+// Load Header
+get_header();
 ?>
-<?php get_header(); ?>
 
 <!-- =============================================================================
-      ABOUT
-     ========================================================================== -->
+  ABOUT
+========================================================================== -->
 <div id="about" class="activeCon">
   <div class="aboutContainer">
     <div class="aboutTitle ralelight">LOS RODRIGUEZ DE SINALOA</div>
     <div class="divider"></div>
-    <div class="aboutImage"></div>
+    <div class="aboutImage">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/about.jpg" border="0" />
+    </div>
     <div class="aboutText ralelight group">
       <div class="columText">
-        <p>
-        Orginarios  de Guasave uno de los pequeños pueblos del estado se Sinaloa con mas tradición musical, los hermanos Geovanne “Geo” ( primera voz y acordeon ) y Jose “Checho” ( segunda voz y guitarra ), han estado unidos musicalmente desde 2006.
-        </p>
-        <p>
-        Desde niños escucharon música sierreña, norteña y banda tradicional Sinaloense, lo que los motivo a tomar el acordeon y la guitarra, para interpretar la música que preferida por sus abuelos, consiguiendo pronto distinguirse por su estilo original.
-        </p>
-        <p>
-        Haciendo uso de redes sociales y con la sencillez heredada de su familia, han logrado mantener el contacto con el publico, que los acompaña en todas sus presentaciones.
-        </p>
+        <p>Orginarios  de Guasave uno de los pequeños pueblos del estado se Sinaloa con mas tradición musical, los hermanos Geovanne “Geo” ( primera voz y acordeon ) y Jose “Checho” ( segunda voz y guitarra ), han estado unidos musicalmente desde 2006.</p>
+        <p>Desde niños escucharon música sierreña, norteña y banda tradicional Sinaloense, lo que los motivo a tomar el acordeon y la guitarra, para interpretar la música que preferida por sus abuelos, consiguiendo pronto distinguirse por su estilo original.</p>
+        <p>Haciendo uso de redes sociales y con la sencillez heredada de su familia, han logrado mantener el contacto con el publico, que los acompaña en todas sus presentaciones.</p>
       </div>
       <div class="columText">
-        <p>
-        Recientemente después de algunas semanas en el estudio de grabación, terminaron lo que será su segunda producción musical que sera titulada  MUJERIEGOS A MORIR.
-        </p>
-        <p>
-        Esta producción incluirá una recopilación de sus temas que han sido éxitos  UNDERGOUND  ( la entrevista, si ando en guerra, ojitos negros chinitos ) , asi como el tema Mujeriego a Morir, que empieza a escucharse en las principales radiodifusoras de la Mexico y la Union Americana.
-        </p>
-        <p>
-        Los Rodriguez de Sinaloa, concientes de que les espera un largo camino por recorrer en la carrera musical, disfrutan cada oportunidad de cantar y ejecutar sus instrumentos, siempre complaciendo a todos sus seguidores.
-        </p>
+        <p>Recientemente después de algunas semanas en el estudio de grabación, terminaron lo que será su segunda producción musical que sera titulada  MUJERIEGOS A MORIR.</p>
+        <p>Esta producción incluirá una recopilación de sus temas que han sido éxitos  UNDERGOUND  ( la entrevista, si ando en guerra, ojitos negros chinitos ) , asi como el tema Mujeriego a Morir, que empieza a escucharse en las principales radiodifusoras de la Mexico y la Union Americana.</p>
+        <p>Los Rodriguez de Sinaloa, concientes de que les espera un largo camino por recorrer en la carrera musical, disfrutan cada oportunidad de cantar y ejecutar sus instrumentos, siempre complaciendo a todos sus seguidores.</p>
       </div>
     </div>
   </div>
